2450c6a7b386fa593b439b5e8346b48b359b76cf
Jonas Auer <jonas.auer.94@gmail.com>
2017-07-08 21:21:35 +0200

    Tear out the MockBackendService again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 
-import { MockBackendService } from './mock-backend.service'
 import { AuthenticationService } from './authentication.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [
-    MockBackendService,
-    AuthenticationService
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-
-  constructor(
-    private mockBackendService: MockBackendService,
-    private authenticationService: AuthenticationService
-  ) {
-    this.mockBackendService.start()
-  }
 }
