e7cc74d3dd193afcc95a320a7d72c3e234f102e8
Jonas Auer <jonas.auer.94@gmail.com>
2017-07-09 00:16:31 +0200

    Add UserOverviewComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AuthenticationService } from './authentication.service';
-import { UserService } from './user.service'
+import { UserService } from './user.service';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
 import { ObjectKeysPipe } from './object-keys.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    UserOverviewComponent,
     ObjectKeysPipe
   ],
   imports: [
