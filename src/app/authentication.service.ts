fc52b5f074a2edce3cbeb02c07716e28d0c3daa5
Jonas Auer <jonas.auer.94@gmail.com>
2017-07-08 20:46:01 +0200

    Fix AuthenticationService treating HTTP errors as success

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import { User } from './user'
@@ -14,9 +14,17 @@ export class AuthenticationService {
 
   async login(username: string, password: string): Promise<void> {
     const { token }: { token: string } = await this.http
-      .post(this.tokenEndpoint, JSON.stringify({ username, password }))
+      .post(this.tokenEndpoint, `username=${username}&password=${password}`, new RequestOptions({
+        headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' })
+      }))
       .toPromise()
-      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        if (res.status >= 400) {
+          throw res.json()
+        }
+        return res.json()
+      })
 
     localStorage.setItem('token', token)
   }
