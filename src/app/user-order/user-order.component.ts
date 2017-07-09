2d5c54901dc6b022fd46a4f8ce94f640a0c2a3c8
Jonas Auer <jonas.auer.94@gmail.com>
2017-07-09 02:31:13 +0200

    Add order confirmation modal

--- src/app/user-order/user-order.component.ts
+++ src/app/user-order/user-order.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { SuiModalService, TemplateModalConfig, ModalTemplate } from 'ng2-semantic-ui';
 
 import { Drink } from '../drink'
+
+export interface IConfirmationModal {
+  drink: Drink
+}
 
 @Component({
   selector: 'user-order',
@@ -8,15 +14,28 @@ import { Drink } from '../drink'
   styleUrls: ['./user-order.component.css']
 })
 export class UserOrderComponent implements OnInit {
+  @ViewChild('confirmationTemplate')
+  public confirmationTemplate: ModalTemplate<IConfirmationModal, void, void>
+
   drinks: Drink[] = (() => {
     return [0,1,2,3,4,5,6,7,8].map(id =>
       new Drink(`${id}`, `Drink ${id}`, (id / 8) + 0.7, (id / 8) + 1, 10, null, 0, '')
     )
   })()
 
-  constructor() { }
+  constructor(public modalService: SuiModalService) { }
 
   ngOnInit() {
   }
 
+  onClick(drink: Drink) {
+    const config = new TemplateModalConfig<IConfirmationModal, void, void>(this.confirmationTemplate)
+
+    config.context = { drink }
+    config.size = 'small'
+
+    this.modalService
+      .open(config)
+      .onApprove(() => console.log('approved'))
+  }
 }
